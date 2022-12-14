a6e553ed26b4550359a76cacc9afded9b62c307c
rion <riiiiion@gmail.com>
2022-12-14 22:53:46 +0900

    feat:ダミーデータをaxiosで取得できるようになった

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import StartModal from './component/StartModal';
-import ResultModal from './component/ResultModal';
-import TypingArea from './component/TypingArea';
-
+import React, { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+import StartModal from "./component/StartModal";
+import ResultModal from "./component/ResultModal";
+import TypingArea from "./component/TypingArea";
 
 function App() {
-const [startModalState, setStartModalState] = useState<Boolean>(true)
-const [resultModalState, setResultModalState] = useState<Boolean>(false)
+  const [startModalState, setStartModalState] = useState<Boolean>(true);
+  const [resultModalState, setResultModalState] = useState<Boolean>(false);
+  const [typingWordList, setTypingWordList] = useState<String[] | null>(null);
+  
+  
+  useEffect(() => {
+    axios.get(`dummyData/dummy1.json`).then((res: AxiosResponse) => {
+      setTypingWordList(res.data);
+      console.log(res);
+    })
+  }, []);
 
-
+ 
 
   return (
     <div className="App">
-      
-    <StartModal startModalState = {startModalState} setStartModalState = {setStartModalState}/>
-    <ResultModal resultModalState = {resultModalState} setResultModalState = {setResultModalState}  setStartModalState = {setStartModalState}/>
-    <TypingArea />
+      <button> 問題取得</button>
+      <StartModal
+        startModalState={startModalState}
+        setStartModalState={setStartModalState}
+      />
+      <ResultModal
+        resultModalState={resultModalState}
+        setResultModalState={setResultModalState}
+        setStartModalState={setStartModalState}
+      />
+      <TypingArea />
     </div>
   );
 }
